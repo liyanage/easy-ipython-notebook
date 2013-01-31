dcbe6326b136892848677f095d089ed1b616f702
Marc Liyanage <liyanage@liyanage1.apple.com>
2013-01-31 10:38:25 -0800

    First checkin

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a simple script to install and use IPython's notebook feature. IPython notebook is a
+great way to interactively explore Python.
+
+Usage:
+
+Clone the project from github:
+
+    git clone https://github.com/liyanage/easy-ipython-notebook.git
+    cd easy-ipython-notebook
+
+Run the combined installer/launcher script:
+
+    ./easy-ipython-notebook.sh
+
+This will download and install IPython and some required modules into a virtualenv
+and launch the notebook feature.
---
+++ easy-ipython-notebook.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+# System-wide install of PIP, if it's not already available
+if ! type pip >/dev/null 2>&1; then
+	sudo easy_install pip
+fi
+
+# System-wide install of virtualenv, if it's not already available
+if ! type virtualenv >/dev/null 2>&1; then
+	sudo pip install virtualenv
+fi
+
+# Initialize a virtualenv if not already present
+if ! [ -e .Python ]; then
+    virtualenv .
+fi
+
+# Activate the virtualenv
+. bin/activate
+
+# Install Python modules required for ipython notebook into virtualenv, if not already present
+for module in Jinja2 pyzmq tornado; do
+    if ! pip freeze | grep -q $module; then
+        pip install $module
+    fi
+done
+
+# Fetch iPython sources, if not already present
+if ! [ -e ipython ]; then
+    git clone https://github.com/ipython/ipython.git
+fi
+
+PYTHONPATH=$PWD/ipython python ipython/ipython.py notebook
+
